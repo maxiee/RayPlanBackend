13181589aca646dcc55caae19eee730862ba66a9
Maxiee <maxieewong@gmail.com>
2021-01-30 21:55:33 +0800

    project model

--- app/project/admin.py
+++ app/project/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from project.models import Task
+from project.models import Task, Project
 
 # Register your models here.
 admin.site.register(Task)
+admin.site.register(Project)
--- app/project/models.py
+++ app/project/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
 # Create your models here.
+PROJECT_STATUS = (
+    ("Todo", "待开始"), 
+    ("Doing", "进行中"),
+    ("Finish", "已完成"),
+    ("Changed", "已变更")
+)
+
+class Project(models.Model):
+    """
+    任务表示一件要做的事情。是最小粒度的行动，不可拆分。
+    """
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=32,
+        choices = PROJECT_STATUS
+    )
+    start = models.DateField(null=True)
+    deadline = models.DateField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
 TASK_STATUS = (
     ("Todo", "待开始"), 
     ("Doing", "进行中"),
