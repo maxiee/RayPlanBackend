4dea837f90743d364a1e16c4aca5fe8ddbc84def
Maxiee <maxieewong@gmail.com>
2021-01-30 21:49:04 +0800

    task model

--- app/project/models.py
+++ app/project/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+TASK_STATUS = (
+    ("Todo", "待开始"), 
+    ("Doing", "进行中"),
+    ("Finish", "已完成"),
+    ("Changed", "已变更")
+)
+
+class Task(models.Model):
+    """
+    任务表示一件要做的事情。是最小粒度的行动，不可拆分。
+    """
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=32,
+        choices = TASK_STATUS
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
